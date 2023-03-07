d001d96b48e682264d94af5813816f86493a52e6
MikalaiKholin <kholin.mikalai@gmail.com>
2023-03-08 00:14:09 +0300

    init: first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int number = 15;
+
+        for (int i = 1; i * i <= number; i++) {
+            if (i * i == number) System.out.printf("nearest square number is %d%n", number);
+            if (i * i < number && (i + 1) * (i + 1) > number) {
+                if (number - i * i < (i + 1) * (i + 1) - number)
+                    System.out.printf("nearest square number is %d%n", i * i);
+                else System.out.printf("nearest square number is %d%n", (i + 1) * (i + 1));
+            }
+        }
     }
 }
---
+++ src/n1.java
@@ -0,0 +1,18 @@
+public class n1 {
+    public static void main(String[] args) {
+        int number = 9;
+        int start = 1;
+        while (start * start < number){
+            start++;
+        }
+        System.out.println(start + " after while"); ///сколько прошло циклов
+
+        if (number == start*start) {
+            System.out.println(start + " == ");
+        }else if ((start*start - number) < (number-(start -1)*(start -1))) {
+            System.out.println(start * start );
+        }else {
+            System.out.println((start - 1) * (start - 1) + " result");
+        }
+    }
+}
